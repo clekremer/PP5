7168c6b52e6012b373a31ec178997ed863937e77
clemens kremer <molkrich@gmx.de>
2024-11-09 06:41:50 +0000

    Add page leaves visualizer page

---
+++ app_pages/page_leaves_visualizer.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import os
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib.image import imread
+
+import itertools
+import random
+
+def page_cells_visualizer_body():
+    st.write("### Leaves Visualizer")
+    st.info(
+        f"* The client is interested in having a study that visually "
+        f"differentiates a parasitised from an healthy leaves.")
