0f8c5edbdab79d067e5d837f68a66870a85eeb51
clemens kremer <molkrich@gmx.de>
2024-11-13 09:17:51 +0000

    Add function on leaves visualizer to display difference between averag and variability image

--- app_pages/page_leaves_visualizer.py
+++ app_pages/page_leaves_visualizer.py
@@ -14,3 +14,20 @@ def page_leaves_visualizer_body():
     st.info(
         f"* The client is interested in having a study that visually "
         f"differentiates a parasitised from an healthy leaves.")
+
+    
+    version = 'v1'
+    if st.checkbox("Difference between average and variability image"):
+      
+      avg_mildew = plt.imread(f"outputs/{version}/avg_var_powdery_mildew.png")
+      avg_healthy = plt.imread(f"outputs/{version}/avg_var_healthy.png")
+
+      st.warning(
+        f"* We notice the average and variability images did not show "
+        f"patterns where we could intuitively differentiate one from another. " 
+        f"However, a small difference in the color pigment of the average images are seen for both labels.")
+
+      st.image(avg_mildew, caption='Mildew leaf - Average and Variability')
+      st.image(avg_healthy, caption='Healthy leaf - Average and Variability')
+      st.write("---")
+
